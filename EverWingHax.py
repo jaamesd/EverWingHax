f0bc6fc65244426e7e41e915eb35375c7598bb01
isometric <james.deng@alumni.ubc.ca>
2016-12-19 19:43:44 -0800

    why bother with rares when it's worse and slows everything

--- EverWingHax.py
+++ EverWingHax.py
@@ -152,7 +152,6 @@ def acquire_sidekicks():
         complete_games(10)
         acquire_eggs("epic", 80)
         acquire_dragons("legendary", 8)
-        acquire_dragons("rare", 8)
         print(" DONE")
         if i % 4:
             level_up_sidekicks()
